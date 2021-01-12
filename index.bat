ae256c3cbaafec9baa625f1c85f800f83706d1e0
Krzem <krzem5.dev@gmail.com>
2021-01-12 19:58:58 +0100

    Push Update 01/12/2021, 19:51:23

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/canvas/Canvas.java&&java com/krzem/canvas/Canvas
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/canvas/Main.java&&jar cvmf ../manifest.mf ../build/canvas.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="canvas.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/canvas.jar
+:end
