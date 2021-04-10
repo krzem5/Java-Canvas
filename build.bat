188eefe718e856d13c28372803af872b7ee18b3b
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:17:31 +0200

    Push Update 04/10/2021, 09:17:13

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/canvas/Main.java&&jar cvmf ../manifest.mf ../build/canvas.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="canvas.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/canvas.jar
-:end
+python build.py %*
